de918ab851627776abad3b1da83538a1e57775d7
admin <mankum08071998@gmail.com>
2021-04-06 15:39:33 +0530

    dot drawables added

--- app/src/main/java/com/example/viewpager2withrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/viewpager2withrecyclerview/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     ViewPager2 mViewpager;
     MyAdapter myAdapter;
     List<String> strings;
+    TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mViewpager = findViewById(R.id.viewPagerEx);
+        mTabLayout = findViewById(R.id.tabLayoutPromotions);
         strings = new ArrayList<>();
         strings.add("Screen 1");
         strings.add("Screen 2");
@@ -35,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         myAdapter = new MyAdapter(this, strings);
         mViewpager.setOrientation(ViewPager2.ORIENTATION_VERTICAL);
         mViewpager.setAdapter(myAdapter);
+        new TabLayoutMediator(mTabLayout, mViewpager, ((tab, position) -> {})).attach();
 
     }
 }
